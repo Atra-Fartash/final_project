dbc7ecb432d43bd8937083b6ab15846663457210
Atra-Fartash <atrafartash@gmail.com>
2025-11-21 16:28:47 +0330

    convert API views to ViewSet

--- courses/views.py
+++ courses/views.py
@@ -1,61 +1,47 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters
+from rest_framework import filters, viewsets
 from courses.serializers import CategorySerializer, TeacherSerializer, CourseSerializer, CommentSerializer, TicketSerializer, TicketMessageSerializer, GroupMemberSerializer
 from courses.models import Category, Teacher, Course, Comment, Ticket, TicketMessage, GroupMembers
 from rest_framework import permissions
 
 
 
-class CategoryListCreate(ListCreateAPIView):
+class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     search_fields = ['name']
 
     def get_permissions(self):
+
         if self.request.method == 'POST':
            return [permissions.IsAdminUser()]
-        return [permissions.IsAuthenticated()]
-
-
-class CategoryRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
-    search_fields = ['name']
-
-    def get_permissions(self):
+        
         if self.request.method == ['PATCH', 'PUT', 'DELETE']:
            return [permissions.IsAdminUser()]
-        return [permissions.AllowAny()]
+        
+        return [permissions.IsAuthenticated()]
     
 
-class TeacherListCreate(ListCreateAPIView):
+class TeacherViewSet(viewsets.ModelViewSet):
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     search_fields = ['name']
 
     def get_permissions(self):
+
         if self.request.method == 'POST':
            return [permissions.IsAdminUser()]
-        return [permissions.IsAuthenticated()]
-
-
-class TeacherRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
-    queryset = Teacher.objects.all()
-    serializer_class = TeacherSerializer
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
-    search_fields = ['name']
-
-    def get_permissions(self):
+        
         if self.request.method == ['PATCH', 'PUT', 'DELETE']:
            return [permissions.IsAdminUser()]
-        return [permissions.AllowAny()]
+        
+        return [permissions.IsAuthenticated()]
     
 
-class CourseListCreate(ListCreateAPIView):
+class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
@@ -64,33 +50,18 @@ class CourseListCreate(ListCreateAPIView):
     ordering_fields = ['price', 'start_date', 'duration']
 
     def get_permissions(self):
+
         if self.request.method == 'POST':
            return [permissions.IsAdminUser()]
-        return [permissions.IsAuthenticated()]
-    
-
-class CourseRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
-    queryset = Course.objects.all()
-    serializer_class = CourseSerializer
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    search_fields = ['title']
-    filterset_fields = ['category']
-    ordering_fields = ['price', 'start_date', 'duration']
-
-    def get_permissions(self):
+        
         if self.request.method == ['PATCH', 'PUT', 'DELETE']:
            return [permissions.IsAdminUser()]
-        return [permissions.AllowAny()]
+        
+        return [permissions.IsAuthenticated()]
 
 
-class GroupMemberListCreate(ListCreateAPIView):
+class GroupMemberViewSet(viewsets.ModelViewSet):
     queryset =  GroupMembers.objects.all()
-    serializer_class = GroupMemberSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-
-class GroupMemberRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
-    queryset = GroupMembers.objects.all()
     serializer_class = GroupMemberSerializer
     permission_classes = [permissions.IsAuthenticated]
 
@@ -114,15 +85,7 @@ class CommentRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
         return Comment.objects.filter(user=user)
     
 
-class TicketListCreate(ListCreateAPIView):
-    permission_classes = [permissions.IsAuthenticated]
-    queryset = Ticket.objects.all()
-    serializer_class = TicketSerializer
-    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
-    ordering_fields = ['created_at']
-
-
-class TicketRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
+class TicketViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
